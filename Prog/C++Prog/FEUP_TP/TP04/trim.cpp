9f58d1163283898c29898793725b0199dc06254f
qqqquadruplesec <148462965+qqqquadruplesec@users.noreply.github.com>
2024-03-11 12:16:25 +0000

    removed debug prints from trim.cpp

--- Prog/C++Prog/FEUP_TP/TP04/trim.cpp
+++ Prog/C++Prog/FEUP_TP/TP04/trim.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 void trim(char s[]) {
-  using namespace std;
   int len = -1, i = 0;
 
   //compute str length.
@@ -12,23 +11,15 @@ void trim(char s[]) {
   
   //loops to check for leading & trailing spaces 
   while (len >= 0 && s[len] == ' ') {
-    //cout << "Current str: " << '"' << s << '"' << endl << "Current length: " << len << endl;
     s[len] = 0;
     len--;
   }
   
   while (len >= 0 && s[0] == ' ') {
-    //cout << "Current str: " << s << endl << "Current length: " << len << endl;
     for (int i = 0; i < len; i++) {
       s[i] = s[i + 1];
     }
     s[len] = 0;
     len--;
   }
-}
-int main(void) {
-  char s[] = "   ";
-  trim(s);
-  std::cout << '"' << s << '"' << '\n';
-  return 0;
 }
